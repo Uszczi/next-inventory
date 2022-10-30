7577446f593de176e2b46b90a4be1666593723df
Uszczi <mateuszpapuga24@gmail.com>
2022-10-30 17:16:42 +0100

    add movies to main page

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -4,7 +4,6 @@ import { PrismaClient } from "@prisma/client";
 export default async function MoviesList() {
   const prisma = new PrismaClient();
   const toDisplay = await prisma.movie.findMany();
-  console.log(toDisplay);
 
   return (
     <div className="flex justify-center overflow-y-scroll        h-[92vh]">
