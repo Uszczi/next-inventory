84cdd68d5000a35a5335bcf3434544be739342b7
Uszczi <mateuszpapuga24@gmail.com>
2022-10-30 17:47:04 +0100

    improve input

--- components/FzfMoviesTable.tsx
+++ components/FzfMoviesTable.tsx
@@ -46,7 +46,7 @@ export default function FzfMoviesTable({ elements }: { elements: Movie[] }) {
     }
 
     const fzf = new Fzf(elements, {
-      limit: 22,
+      limit: 25,
       casing: "case-insensitive",
       selector: (item) => item.title,
     });
@@ -54,8 +54,19 @@ export default function FzfMoviesTable({ elements }: { elements: Movie[] }) {
   }, [elements]);
 
   return (
-    <div className="" style={{ width: "700px" }}>
-      <input placeholder="Start typing..." onChange={handleInput} />
+    <div className="w-[700px]">
+      <div className="mb-4 mt-7">
+        <div className="relative w-full  h-10">
+          <input
+            className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2  focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
+            placeholder=" "
+            onChange={handleInput}
+          />
+          <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-blue-gray-400 peer-focus:text-blue-500 before:border-blue-gray-200 peer-focus:before:border-blue-500 after:border-blue-gray-200 peer-focus:after:border-blue-500">
+            Start typing ...
+          </label>
+        </div>
+      </div>
       {toDisplay.map((element, i) => {
         return (
           <div key={i} className="flex shadow-md mb-5 p-4 items-center gap-3">
@@ -68,7 +79,7 @@ export default function FzfMoviesTable({ elements }: { elements: Movie[] }) {
               onClick={() => {}}
             />
             <p className="font-bold">{element.item.directors}</p>
-            <p className="ml-auto">{element.item.watch_date}</p>
+            <p className="ml-auto shrink-0">{element.item.watch_date}</p>
           </div>
         );
       })}
