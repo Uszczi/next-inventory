3917742695721040e0a5e1b6f005b64f85ef2181
Uszczi <mateuszpapuga24@gmail.com>
2022-10-30 15:32:19 +0100

    add fzf

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,33 +1,4 @@
-// import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import MoviesService, { Movie } from "../services/movies";
-// import "./MoviesTable.css";
-//
-// import { Fzf, FzfResultItem } from "fzf";
-// import { useDispatch, useSelector } from "react-redux";
-// import { set as moviesStoreSet } from "../store/reducers/movies";
-//
-// const HighlightChars = (props: {
-//   str: string;
-//   positions: Set<number>;
-//   onClick: any;
-// }) => {
-//   const chars = props.str.split("");
-//
-//   const nodes = chars.map((char, i) => {
-//     if (props.positions.has(i)) {
-//       return <span key={i}>{char}</span>;
-//     } else {
-//       return char;
-//     }
-//   });
-//
-//   return <p onClick={props.onClick}>{nodes}</p>;
-// };
-//
-//
-
-//
+import FzfMoviesTable from "../../components/FzfMoviesTable";
 
 export interface Movie {
   directors: string;
@@ -43,65 +14,10 @@ async function getMovies(): Promise<Movie[]> {
 }
 export default async function MoviesList() {
   let toDisplay = await getMovies();
-  // toDisplay = toDisplay.slice(0, 100);
-  // const toDisplay = [];
-  // const toDisplay = [
-  //   {
-  //     directors: "Lana Wachowski, Lilly Wachowski",
-  //     id: "6329d3f87cb475833c0b2322",
-  //     production_year: "1999",
-  //     title: "Matrix",
-  //     watch_date: "2021-01-01",
-  //   },
-  // ];
-  // const [fzf, setFzf] = useState({} as Fzf<Movie[]>);
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (movies.length > 0) {
-  //     const allFzf = new Fzf(movies, {
-  //       fuzzy: "v1",
-  //       selector: (item) => item.title,
-  //     });
-  //     const allItems = allFzf.find("");
-  //     setToDisplay(allItems);
-  //
-  //     const fzf = new Fzf(movies, {
-  //       limit: 22,
-  //       casing: "case-insensitive",
-  //       selector: (item) => item.title,
-  //     });
-  //     setFzf(fzf);
-  //   } else {
-  //     MoviesService.getMovies().then((movies) =>
-  //       dispatch(moviesStoreSet(movies))
-  //     );
-  //   }
-  // }, [movies]);
-  //
-  // function handleInput(e: React.FormEvent<HTMLInputElement>) {
-  //   const result = fzf.find(e.currentTarget.value);
-  //   setToDisplay(result);
-  // }
 
   return (
-    <div className="flex justify-center overflow-y-scroll        h-[92vh] ">
-      <div className="" style={{ maxWidth: "700px" }}>
-        <input placeholder="Start typing..." />
-
-        {toDisplay.map((element, i) => {
-          return (
-            <div key={i} className="flex shadow-md mb-5 p-4 items-center gap-3">
-              <div className="bg-black w-8 h-8 text-white rounded-full flex items-center justify-center">
-                {i + 1}
-              </div>
-              <p>{element.title}</p>
-              <p className="font-bold">{element.directors}</p>
-              <p className="ml-auto">{element.watch_date}</p>
-            </div>
-          );
-        })}
-      </div>
+    <div className="flex justify-center overflow-y-scroll        h-[92vh]">
+      <FzfMoviesTable elements={toDisplay} />
     </div>
   );
 }
---
+++ components/FzfMoviesTable.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Fzf, FzfResultItem } from "fzf";
+import { useEffect, useState } from "react";
+
+export interface Movie {
+  directors: string;
+  id: string;
+  production_year: string;
+  title: string;
+  watch_date: string;
+}
+
+const HighlightChars = (props: {
+  str: string;
+  positions: Set<number>;
+  onClick: any;
+}) => {
+  const chars = props.str.split("");
+
+  const nodes = chars.map((char, i) => {
+    if (props.positions.has(i)) {
+      return (
+        <span className="text-amber-500" key={i}>
+          {char}
+        </span>
+      );
+    } else {
+      return char;
+    }
+  });
+
+  return <p onClick={props.onClick}>{nodes}</p>;
+};
+
+export default function FzfMoviesTable({ elements }: { elements: Movie[] }) {
+  const [fzf, setFzf] = useState({} as Fzf<Movie[]>);
+  const [toDisplay, setToDisplay] = useState([] as FzfResultItem<Movie>[]);
+
+  function handleInput(e: React.FormEvent<HTMLInputElement>) {
+    const result = fzf.find(e.currentTarget.value);
+    setToDisplay(result);
+  }
+
+  useEffect(() => {
+    if (elements.length > 0) {
+      const allFzf = new Fzf(elements, {
+        fuzzy: "v1",
+        selector: (item) => item.title,
+      });
+      const allItems = allFzf.find("");
+      setToDisplay(allItems);
+    }
+
+    const fzf = new Fzf(elements, {
+      limit: 22,
+      casing: "case-insensitive",
+      selector: (item) => item.title,
+    });
+    setFzf(fzf);
+  }, []);
+
+  return (
+    <div className="" style={{ width: "700px" }}>
+      <input placeholder="Start typing..." onChange={handleInput} />
+      {toDisplay.map((element, i) => {
+        return (
+          <div key={i} className="flex shadow-md mb-5 p-4 items-center gap-3">
+            <div className="bg-black w-8 h-8 text-white rounded-full flex items-center justify-center">
+              {i + 1}
+            </div>
+            <HighlightChars
+              str={element.item.title}
+              positions={element.positions}
+              onClick={() => {}}
+            />
+            <p className="font-bold">{element.item.directors}</p>
+            <p className="ml-auto">{element.item.watch_date}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
