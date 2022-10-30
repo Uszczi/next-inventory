bce5ce59378386170a2f58018ffc49a2b6e9c122
Uszczi <mateuszpapuga24@gmail.com>
2022-10-30 16:39:07 +0100

    add prisma

---
+++ scripts/load_example_movies.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var movies_1 = require("../example_data/movies");
+console.log(movies_1["default"]);
---
+++ scripts/load_example_movies.ts
@@ -0,0 +1,6 @@
+// import movies from "../example_data/movies";
+
+// console.log(movies);
+console.log("hello world");
+
+export {};
